4d323d165bfd9c81ebeb6ef0f70cb2aa004f07ce
Alok Kumar Singh <alokkumarsingh@Aloks-MacBook-Pro.local>
2020-03-20 16:33:52 +0530

    fix non fictional issue

--- js/main.js
+++ js/main.js
@@ -1,12 +1,48 @@
-function openBook(evt, cityName) {
-    var i, tabcontent, tablinks;
-  
-    tabcontent = document.getElementsByClassName("speech-bubble");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-  
-    tablinks = document.getElementById(cityName);
-    tablinks.style.display='block';
-    
+function openBook(evt, book) {
+  var i, tabcontent, tablinks;
+
+  tabcontent = document.getElementsByClassName("speech-bubble");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
   }
+
+  tablinks = document.getElementById(book);
+  tablinks.style.display = 'block';
+
+}
+
+function changeTabLink(e) {
+  const links = Array.from(document.getElementsByClassName('links'))
+  links.forEach(link=>{
+    if(link === e.target){
+      link.classList.add('isactive')
+    }else{
+      link.classList.remove('isactive')
+    }
+  })
+}
+
+function openTab(event, id) {
+
+  var i, tabcontent, tablinks;
+
+  tabcontent = document.getElementsByClassName("books");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].classList.add('hidden')
+  }
+
+  document.getElementById(id).classList.remove("hidden")
+
+}
+
+
+window.addEventListener('load', () => {
+  document.getElementById('default').click()
+  const links = Array.from(document.getElementsByClassName('links'))
+  links.forEach(link=>{
+    link.addEventListener('click',changeTabLink)
+  })
+})
+
+
+
